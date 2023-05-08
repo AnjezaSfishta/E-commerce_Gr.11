e92fcae6415dc1b938cd8548aa295f772654617c
Anila Luta <118193110+AnilaLuta@users.noreply.github.com>
2023-05-08 15:51:39 +0200

    Update edit.php

--- dashboard/categories/edit.php
+++ dashboard/categories/edit.php
@@ -1,1 +1,28 @@
+<?php 
+    include('../includes/header.php'); 
+    include('../../classes/CRUD.php');
+    
+    $crud = new CRUD;
+    $category = $crud->read('categories', ['column' => 'id', 'value' => $_GET['id']] ,1);
 
+    $errors = [];
+
+    if(isset($_POST['update_category_btn'])) {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+
+        if(empty($name)) {
+            $errors[] = 'Name is empty!';
+        }
+
+        if(empty($id)) {
+            header('Location: index.php');
+        }
+
+        if($crud->update('categories', ['name' => $name], ['column' => 'id', 'value' => $id]) === true) {
+            header('Location: index.php?action=update&status=success');
+        } else {
+            $errors = 'Something want wrong!';
+        }
+    }
+?>
