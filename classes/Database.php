cdb950cd7ecbd5b12c6e2baa7d4c0bf23f628fbe
arlindnimani1 <100229702+arlindnimani1@users.noreply.github.com>
2023-05-08 15:39:38 +0200

    Update Database.php

--- classes/Database.php
+++ classes/Database.php
@@ -1,1 +1,23 @@
 
+<?php
+
+class Database {
+    private static $instance = null;
+    private $connection = null;
+
+    public function __construct() {
+        $this->connection = new mysqli('localhost:3307', 'root', '', 'onlinestore');
+    }
+
+    public static function getInstance() {
+        if(!self::$instance) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    function getConnection() {
+        return $this->connection;
+    }
+}
