2e158f7383c835036436d67f20deabab8ad6a7e7
Anila Luta <118193110+AnilaLuta@users.noreply.github.com>
2023-05-13 21:32:19 +0200

    Update edit.php

--- dashboard/categories/edit.php
+++ dashboard/categories/edit.php
@@ -26,3 +26,33 @@
         }
     }
 ?>
+<div class="dashboard my-5">
+    <div class="container">
+        <h3 class="mb-4">Update category</h3>
+        <div class="card">
+            <div class="card-body">
+                <?php if($errors): ?>
+                    <ul>
+                    <?php foreach($errors as $error): ?>
+                        <li><?= $error ?></li>
+                    <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+                <?php if(isset($category) && is_array($category[0])): ?>
+                <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                    <div class="form-group mb-4">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" value="<?= $category[0]['name'] ?>" class="form-control" required="">
+                    </div>
+                    <input type="hidden" name="id" value="<?= $category[0]['id'] ?>">
+                    <button type="submit" class="btn btn-primary" name="update_category_btn">Update</button>
+                </form>
+                <?php else: ?>
+                    <p>Category doesn't exist!</p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('../includes/footer.php'); ?>
